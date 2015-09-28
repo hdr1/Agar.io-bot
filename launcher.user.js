430064b7e70f3201fec53cba1b43d8a0765d2654
hdr1 <max.wm@hotmail.com>
2015-09-27 22:25:18 -0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1310,7 +1310,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["god"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
